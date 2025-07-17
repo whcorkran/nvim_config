54e572ae84d1a18a07398d27188d1ec87b4002c9
whcorkran <hcorkran@seas.upenn.edu>
2025-07-16 22:40:59 -0400

    edit telescope config

--- lua/config_master/plugins/telescope.lua
+++ lua/config_master/plugins/telescope.lua
@@ -2,3 +2,4 @@ local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>fz', builtin.find_files, {})
 vim.keymap.set('n', '<leader>gz', builtin.git_files, {})
 vim.keymap.set('n', '<leader>grp', builtin.live_grep, {})-- needs ripgrep installed with brew
+vim.keymap.set('n', '<leader>grpb', builtin.current_buffer_fuzzy_find, {})
